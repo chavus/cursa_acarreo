1b0b3a231caf63303fd33616ad1bd0478e3e28e1
chavus <nojiso@gmail.com>
2020-05-28 18:03:00 -0500

    "set connect:false to mongodb settings"

--- cursa_acarreo/__init__.py
+++ cursa_acarreo/__init__.py
@@ -12,7 +12,8 @@ locale.setlocale(locale.LC_ALL, 'es_ES')
 
 
 # Database setup
-app.config['MONGODB_SETTINGS'] = {'host': 'mongodb+srv://dbuser:sa170687@cluster0-atrnj.mongodb.net/general?retryWrites=true&w=majority'}
+app.config['MONGODB_SETTINGS'] = {'host': 'mongodb+srv://dbuser:sa170687@cluster0-atrnj.mongodb.net/general?retryWrites=true&w=majority',
+                                  'connect': False}
 app.config['SECRET_KEY'] = 'secretkey'
 
 
