3d986334d74b9b015b7ac562cd027071b9fb8341
chavus <nojiso@gmail.com>
2020-08-28 18:16:53 -0500

    Changing flask hostname

--- app_cursa_acarreo.py
+++ app_cursa_acarreo.py
@@ -6,4 +6,4 @@ v1.2 - 09/13/2020 - Printing ticket and scanning tickets
 
 from cursa_acarreo import app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,  debug=True)
+    app.run(port=5000,  debug=True)
